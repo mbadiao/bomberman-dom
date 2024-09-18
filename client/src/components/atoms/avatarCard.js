f16d3236fd4f4c7b56afe095d9cc805a72bdbefc
jefaye <clement.jean.l.faye@gmail.com>
2024-09-18 08:19:32 +0000

    ...

--- client/src/components/atoms/avatarCard.js
+++ client/src/components/atoms/avatarCard.js
@@ -1,20 +1,11 @@
 import VirtualNode from "../../core/node.js";
 
-// const avartarCard = new VirtualNode({
-//     tag: "div",
-//     attrs: {
-//         class: "avatar-card",
-//         style : "width: 100px; height: 100px; border-radius: 50%; background-color: #f1f1f1; display: flex; justify-content: center; align-items: center; font-size: 2rem; color: #000; font-weight: bold;"
-//     }
-// })
-
 class avartarCard extends VirtualNode {
     constructor(emoji) {
         super({
             tag: "div",
             attrs: {
                 class: "avatar-card",
-                style: "width: 100px; height: 100px; border-radius: 50%; margin: 7px;  background-color: #f1f1f1; display: flex; justify-content: center; align-items: center; font-size: 4rem; color: #000; font-weight: bold;"
             },
             children: [emoji],
         })
