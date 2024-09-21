b3acfc03fb30a56d53ad7b5210ea9a059641c042
jefaye <clement.jean.l.faye@gmail.com>
2024-09-21 02:53:14 +0000

    Ending Cover [OPTIMIZED]...

--- client/src/components/pages/end-game.js
+++ client/src/components/pages/end-game.js
@@ -2,14 +2,13 @@ import Cover from "../molecules/cover.js";
 import Image from "../atoms/image.js";
 import Heading from "../atoms/heading.js";
 import Span from "../atoms/span.js";
-// import Button from "../atoms/button.js";
 
 export default () => {
-
     let ok = false
     document.body.innerHTML = '';
-    document.body.appendChild(new Cover(new Cover(
-        // <img>
+
+    const endCover = new Cover(new Cover(
+      // <img>
         new Image("logo", "./assets/man.svg", {
           width: "200",
           height: "200",
@@ -22,10 +21,7 @@ export default () => {
             return new Span("letter", letter);
           })
         ),
-  
-        // // <button>
-        // new Button(() => {
-        //   window.location.hash = "/insert";
-        // }, ["Start"])
-      )))
+      ))
+      
+      document.body.appendChild(endCover.render())
 }
